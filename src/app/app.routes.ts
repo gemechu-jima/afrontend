f2d2b844f611319d53b350c27c5dd25e7284d0ae
gemechu-jima <gemechujimacs@gmail.com>
2026-06-15 04:33:30 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import {AuthComponent} from "./features/auth/pages/auth.component"
+import { HomeComponent } from './features/home/home.component';
+import { AboutComponent } from './features/about/about.component';
+import { ContactComponent } from './features/contact/contact.component';
+export const routes: Routes = [
+    { path:"login", component:AuthComponent},
+    {path:"", component: HomeComponent},
+    {path:"contact", component:ContactComponent},
+    {path:"about", component:AboutComponent}
+];
